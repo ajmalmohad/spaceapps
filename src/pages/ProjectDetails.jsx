6fe6c7f439e1b589ff9818ea6dfb72d83bdee48d
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-10-08 02:22:28 +0530

    Project Details

--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -1,8 +1,12 @@
-import { Card, Image, CardFooter, Chip, Divider, Button } from '@nextui-org/react'
-import React from 'react'
+import { Card, Image, CardFooter, Chip, Divider, Button, Avatar } from '@nextui-org/react'
+import React, { useState } from 'react'
 import Biology from './../assets/Biology.jpg'
+import Requests from './../components/Requests'
 
 function ProjectDetails() {
+
+  let [isMine, setIsMine] = useState(true);
+
   return (
     <div className='ProjectDetails p-[40px] w-full'>
 
@@ -25,23 +29,45 @@ function ProjectDetails() {
       />
     </Card>
     
-    <div className='flex'>
+    <div className='flex text-white mt-10'>
         <div className='details'>
-            <h1 className='text-xl'>Project Details</h1>
+            <h1 className='text-xl mb-2 font-bold'>Project Details</h1>
             <p>
               Augmented reality (AR) is a technology that blends the digital and physical worlds enhancing our perception of reality by overlaying computer-generated information, such as images,videos, or 3D models, onto our view of the physical environment. Unlike virtual reality, which immerses users in entirely computer-generated environments, AR supplements and enriches the real world.
             </p>
         </div>
         <div className='tags'>
-          <h1 className='text-xl'>Tags</h1>
+          <h1 className='text-xl mb-2 font-bold'>Tags</h1>
           <div className='chips'>
-              <Chip>Augmented Reality</Chip>
-              <Chip>AR</Chip>
-              <Chip>VR</Chip>
+              <Chip color='primary' className='mx-2 my-1'>Augmented Reality</Chip>
+              <Chip color='primary' className='mx-2 my-1'>AR</Chip>
+              <Chip color='primary' className='mx-2 my-1'>VR</Chip>
           </div>
-          <Button>Submit</Button>
+          {!isMine ? <Button className='mt-4 ml-2' color="warning">Send Request</Button> : ""}
         </div>
     </div>
+
+    <div className='collaborators mt-4'>
+        <h1 className='text-xl mb-2 text-white font-bold'>Collaborators</h1>
+        <div className='avatars flex'>
+            <Avatar className='mr-2' src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+            <Avatar className='mr-2' src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+            <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+        </div>
+    </div>
+
+    {
+      isMine ?
+      <div>
+        <h1 className='text-xl my-2 mt-6 text-white font-bold'>Requests</h1>
+        <div className='requests mt-2'>
+            <div className='mt-2'><Requests profileName="Ajmal"/></div>
+            <div className='mt-2'><Requests profileName="Ajmal"/></div>
+            <div className='mt-2'><Requests profileName="Ajmal"/></div>
+        </div>
+      </div>
+      : <></>
+    }
 
     </div>
   )
