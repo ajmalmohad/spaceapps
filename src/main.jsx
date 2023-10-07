520a1f6dfa362eade0a69bcc574837c5f1dd566e
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-10-07 13:37:08 +0530

    Login and Discover

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from "react-router-dom";
 import { AppContextProvider } from './context/AuthContext.jsx';
+import {NextUIProvider} from '@nextui-org/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
+  <NextUIProvider>
     <AppContextProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </AppContextProvider>
-
+  </NextUIProvider>
 )
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import './css/Discover.css'
+import {Input} from "@nextui-org/react";
 
 function Discover() {
   return (
     <div className='Discover'>
       <Navbar />
+      <Input
+        label="Search"
+        isClearable
+        placeholder="Type to search..."
+      />
     </div>
   )
 }
